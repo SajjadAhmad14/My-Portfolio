cbf91f0d984d9ae8016de964ad87e2b29869774a
Sajjad Ahmad <jogimar14@gmail.com>
2022-07-13 17:38:06 +0500

    Add close window button to mobile view

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
-let ham = document.getElementById("see-pro");
-console.log(ham)
-// ham.addEventListner("click", () => {
-//   console.log("hello")
-// })
+const btn = document.getElementById("ham");
+const closeBtn = document.getElementById("close")
+btn.addEventListener(
+  "click",
+  (toggleBtn = () => {
+    btn.style.display = "none";
+    closeBtn.style.display = "block";
+  })
+);
+
+closeBtn.addEventListener(
+    "click",
+    (toggleBtn = () => {
+      closeBtn.style.display = "none";
+      btn.style.display = "block";
+    })
+  );
